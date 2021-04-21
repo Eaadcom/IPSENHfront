84c4de8ea5b33fa05f90cf3ef4fc7cd04d0e8a56
Carlos van der Linden <s1115889@student.hsleiden.nl>
2021-04-21 12:34:14 +0200

    Added starting page example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ipsenh-frontend';
+
+  fetch(): void {
+    this.get().subscribe( obs => {
+      console.log(obs);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  get<T>(): Observable<T> {
+    return this.httpClient.get<T>('localhost:8080');
+  }
 }
