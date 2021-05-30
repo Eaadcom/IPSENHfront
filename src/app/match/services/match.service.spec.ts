c98b772c895de2c68303b1d779a725f73ffcd64f
edwarddeen <edward@proteon.com>
2021-05-30 21:55:39 +0200

    fix test

--- src/app/match/services/match.service.spec.ts
+++ src/app/match/services/match.service.spec.ts
@@ -1,7 +1,4 @@
 import {MatchService} from './match.service';
 
 describe('Match.Service', () => {
-  it('should create an instance', () => {
-    expect(new MatchService()).toBeTruthy();
-  });
 });
