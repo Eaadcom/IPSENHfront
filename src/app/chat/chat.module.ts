396a47f9a2d1870527346edda6df85e575d6465d
Carlos van der Linden <s1115889@student.hsleiden.nl>
2021-06-01 16:09:17 +0200

    removed chat component

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ChatPageComponent} from './pages/chat-page/chat-page.component';
-import {ChatComponent} from './components/chat/chat.component';
 import {LikeMatchListComponent} from './components/like-match-list/like-match-list.component';
 import {MessageListComponent} from './components/message-list/message-list.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -10,7 +9,6 @@ import {NbCardModule, NbChatModule, NbLayoutModule, NbListModule, NbThemeModule,
 @NgModule({
   declarations: [
     ChatPageComponent,
-    ChatComponent,
     LikeMatchListComponent,
     MessageListComponent,
 
