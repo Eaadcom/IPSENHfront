58ef681cd62fa22be06d8db13f9e7c7bd8d1d32b
Joep Oonk <joep_oonk@hotmail.com>
2021-06-01 11:49:52 +0200

    fixed edit form component test.

--- src/app/codesnippet/components/edit-form/edit-form.component.spec.ts
+++ src/app/codesnippet/components/edit-form/edit-form.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditFormComponent } from './edit-form.component';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {CodesnippetService} from '../../services/codesnippet.service';
 
 describe('EditFormComponent', () => {
   let component: EditFormComponent;
   let fixture: ComponentFixture<EditFormComponent>;
+  let service: CodesnippetService;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ EditFormComponent ]
     })
     .compileComponents();
+  });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(CodesnippetService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
