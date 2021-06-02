95940a6c77d986e2fad94b8254680835b2397186
Joep Oonk <joep_oonk@hotmail.com>
2021-06-02 13:55:25 +0200

    fixed test codesnippet-overview.component.spec.ts, imported RouterTestingModule

--- src/app/codesnippet/components/codesnippet-overview/codesnippet-overview.component.spec.ts
+++ src/app/codesnippet/components/codesnippet-overview/codesnippet-overview.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CodesnippetOverviewComponent } from './codesnippet-overview.component';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {ActivatedRoute} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('CodesnippetOverviewComponent', () => {
   let component: CodesnippetOverviewComponent;
   let fixture: ComponentFixture<CodesnippetOverviewComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,6 +16,14 @@ describe('CodesnippetOverviewComponent', () => {
     })
     .compileComponents();
   });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CodesnippetOverviewComponent);
