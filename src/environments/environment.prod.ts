b79bf9c67d6d42c950fcd4c00b0515d53fcefca1
Ashna Wiar <s1114498@student.hsleiden.nl>
2021-06-07 12:18:37 +0200

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   APIEndpoint: 'http://groep-7.xyz:8001/',
+  APIRoutes: {
+    auth: {
+      login: 'auth/login',
+      register: 'auth/register',
+      logout: 'auth/logout',
+      refresh: 'auth/refresh-token',
+    },
+  },
 };
