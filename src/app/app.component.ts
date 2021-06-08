8941d5cd4fde265ebe047f5f45b9db16c1564e23
Carlos van der Linden <s1115889@student.hsleiden.nl>
2021-06-08 23:13:33 +0200

    added navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {NbAuthJWTToken, NbAuthService, NbAuthToken} from '@nebular/auth';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ipsenh-frontend';
+  isAuthenticated: boolean;
+  authSubscription: Subscription;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient,
+              public nbAuthService: NbAuthService) {
+    this.isAuthenticated = false;
+    this.authSubscription = new Subscription();
+  }
+
+  ngOnInit(): void {
+    this.listenIfAuthenticated();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
+
+  listenIfAuthenticated(): void {
+    this.authSubscription = this.nbAuthService.onTokenChange()
+      .subscribe(token => {
+        this.isAuthenticated = this.checkIfAuthenticated(token);
+      });
+  }
+
+  checkIfAuthenticated(token: NbAuthToken): boolean {
+    return token instanceof NbAuthJWTToken && token.isValid();
+  }
 }
 
 
