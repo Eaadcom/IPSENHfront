3f2f691968fbb2327da8fa1d4552eb75d835ae25
Carlos van der Linden <s1115889@student.hsleiden.nl>
2021-06-09 13:10:14 +0200

    added is not authenticated auth guard for the auth routes

---
+++ src/app/authentication/guards/is-not-authenticated.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import {NbAuthService} from '@nebular/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsNotAuthenticatedGuard implements CanActivate {
+
+  constructor(private authenticationService: NbAuthService, private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.canActivateChild(route, state);
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return !this.authenticationService.isAuthenticated() ? true : this.router.parseUrl('/match');
+  }
+}
