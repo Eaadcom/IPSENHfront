884d5fbbe5bebec0157b2df30f84c5e4803233de
Ashna Wiar <s1114498@student.hsleiden.nl>
2021-06-16 22:34:11 +0200

    Add form feedback

--- src/app/user/components/user-edit-form/user-edit-form.component.ts
+++ src/app/user/components/user-edit-form/user-edit-form.component.ts
@@ -11,19 +11,50 @@ export class UserEditFormComponent {
 
   @Input() user: any;
   @Input() submitted: boolean;
+  showMessages: any;
+  error = [] as string[];
+  message = [] as string[];
 
   constructor(
     private userService: UserService,
   ) {
+    this.showMessages = this.initializeShowMessage();
+    this.message = [];
     this.user = {};
     this.submitted = false;
   }
 
   updateUser(): void {
-    this.userService.update(this.user).subscribe();
+    this.userService.update(this.user).subscribe((response) => {
+      this.showMessages.show = false;
+      this.showMessages.success = true;
+      this.message = ['Successfully updated user'];
+    }, error => {
+      this.handleErrors(error.error);
+    });
   }
 
   getOptions(): any {
     return formConfiguration;
+  }
+
+  private handleErrors(errors: any): void {
+    this.error = [];
+    this.showMessages.show = false;
+    this.showMessages.error = true;
+
+    Object.keys(errors).forEach((k) => {
+      this.error = this.error.concat(errors[k]);
+    });
+
+    console.log(this.error);
+  }
+
+  private initializeShowMessage(): any {
+    return {
+      show: true,
+      error: false,
+      success: false,
+    };
   }
 }
